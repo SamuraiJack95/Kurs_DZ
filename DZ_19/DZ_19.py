d09efd2baa92dec4f4446642cff72ff44b2b8a0d
Privalov_A_A <samurai_jack2@mail.ru>
2023-04-21 11:59:38 +0400

    up 19dz

--- DZ_19/DZ_19.py
+++ DZ_19/DZ_19.py
@@ -1,10 +1,33 @@
 import json
 
+# class Loadsave:
+#     def __init__(self):
+#         self.data = {}
+#         self.filepath = 'pip.txt'
+#
+#     def save(self):
+#         self.data[class]
+#     # def add_shoes(self, shoes):
+#     #     self.shoeses[shoes['тип обуви']] = Shoes(*shoes.values())
+#     #     # for i in self.shoeses.items():
+#     #     #     print(i[0], i[1])
+#     #
+#     #     dict_shoeses = {shoes.type_shoes: shoes.__dict__ for shoes in self.shoeses.values()}
+#     #     json.dump(dict_shoeses, open(self.filepath, 'wt', encoding='utf-8'))
+#
+#     def load(self):
+#         pass
+#
+#         # self.shoeses ={}
+#         #             self.shoesess = json.load(open(self.filepath, 'rt', encoding= 'utf-8'))
+#         #             for shoes in self.shoesess.values():
+#         #                 self.shoeses[shoes['type_shoes']] = Shoes(*shoes.values())
+#
 class Shape:
 
     def __init__(self, cor):
         self.cor = cor
-
+        self.cls = self.__class__.__name__
 
     def show(self):
         print(f'Фигура{self.__class__.__name__}')
@@ -12,16 +35,21 @@ class Shape:
         for name, value in self.__dict__.items():
             print(f'{name} = {value}')
 
-    @staticmethod
-    def save(file_pip):
-        data = (?).__dict__
-        with open("file_pip.json", "w") as write_file:
+
+    def save(self, file_pip):
+        data = self.__dict__
+        with open(file_pip, "at") as write_file:
             json.dump(data, write_file)
 
-    @staticmethod
-    def load(file_pip):
-        with open("file_pip.json", "r") as read_file:
+
+    def load(self, file_pip):
+        with open(file_pip, "r") as read_file:
             data = json.load(read_file)
+            for obj in data:
+                for atr in obj:
+                    if atr == self.cls:
+                        return __class__.__name__(*obj.values())
+
 
 class Square(Shape):
 
@@ -47,4 +75,16 @@ class Circle(Shape):
 
 
 a = Circle(4,5)
+b = Square(6,7)
 a.save('pip.txt')
+b.save('pip.txt')
+
+c = Square.load('pip.txt', Square)
+
+
+
+
+
+
+
+
